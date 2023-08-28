66eac91328e50f166745242525b7934716ca57a9
pawgog <pgogola10@gmail.com>
2023-08-28 21:02:24 +0200

    Add function to visible or hidden dropdown menu

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,65 +1,71 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { logout } from '../auth';
 import { staticText } from './staticText';
 
 function NavBar({ loggedIn, onLogout }) {
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
   const handleLogout = () => {
     logout();
     onLogout();
   };
 
-  return (
-<nav>
-  <div className="mx-auto px-2 sm:px-6 lg:px-8">
-    <div className="relative flex h-16 items-center justify-between">
-    <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-        <Link className="navbar-item" to="/">
-          <span className="icon mr-1">
-            <i className="fas fa-home"></i>
-          </span>
-          <span>{staticText.home}</span>
-        </Link>
-        {loggedIn && (
-        <Link className="navbar-item" to="/jobs/new">
-          <span className="icon mr-1">
-            <i className="fa-solid fa-file-circle-plus"></i>
-          </span>
-          <span>{staticText.addJob}</span>
-        </Link>
-        )}        
-      </div>
-      <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-          {loggedIn ? (
-            <div className="relative ml-3">
-              <div>
-                <button type="button" className="relative flex rounded-full bg-gray-800 text-sm" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                  <span className="absolute -inset-1.5"></span>
-                  <span className="sr-only">Open user menu</span>
-                  <img className="h-8 w-8 rounded-full" src="/img/profile.png" alt="profile" />
-                </button>
-              </div>
-              <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1">
-                <a href="/" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-0">Your Profile</a>
-                <div className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-2">
-                  <button className="button is-light" onClick={handleLogout}>
-                    <span className="icon">
-                      <i className="fa-solid fa-arrow-right-from-bracket"></i>
-                    </span>
-                    <span>{staticText.logout}</span>
-                  </button>
-                </div>
-              </div>
-            </div>
-          ) : (
-            <Link className="button is-link is-light" to="/login">
-              <span className="icon">
-                <i className="fa-solid fa-arrow-right-to-bracket"></i>
+  const handleDropdown = () => {
+    setIsDropdownVisible(prev => !prev);
+  };
+
+return (
+    <nav>
+      <div className="mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <Link className="navbar-item" to="/">
+              <span className="icon mr-1">
+                <i className="fas fa-home"></i>
               </span>
-              <span>{staticText.login}</span>
+              <span>{staticText.home}</span>
             </Link>
-          )}
+            {loggedIn && (
+            <Link className="navbar-item" to="/jobs/new">
+              <span className="icon mr-1">
+                <i className="fa-solid fa-file-circle-plus"></i>
+              </span>
+              <span>{staticText.addJob}</span>
+            </Link>
+            )}        
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              {loggedIn ? (
+                <div className="relative ml-3">
+                  <div>
+                    <button type="button" onClick={handleDropdown} className="relative flex rounded-full bg-gray-800 text-sm" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                      <span className="absolute -inset-1.5"></span>
+                      <span className="sr-only">Open user menu</span>
+                      <img className="h-8 w-8 rounded-full" src="/img/profile.png" alt="profile" />
+                    </button>
+                  </div>
+                  <div className={`${isDropdownVisible ? "block" : "hidden"} absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`} role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1">
+                    <a href="/" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-0">Your Profile</a>
+                    <div className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-2">
+                      <button className="button is-light" onClick={handleLogout}>
+                        <span className="icon">
+                          <i className="fa-solid fa-arrow-right-from-bracket"></i>
+                        </span>
+                        <span>{staticText.logout}</span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <Link className="button is-link is-light" to="/login">
+                  <span className="icon">
+                    <i className="fa-solid fa-arrow-right-to-bracket"></i>
+                  </span>
+                  <span>{staticText.login}</span>
+                </Link>
+              )}
+          </div>
         </div>
-      </div>
       </div>
     </nav>
   );
