6431be8dcfdab68bc1ce752ec0712b670cc840c7
pawgog <pgogola10@gmail.com>
2023-09-12 19:34:02 +0200

    Profil page data.

--- client/src/components/Login/ProfilePage.js
+++ client/src/components/Login/ProfilePage.js
@@ -1,9 +1,21 @@
 function ProfilePage() {
   return (
-    <div>
+    <>
       <h2>Profile Page</h2>
-      <p>Content</p>
-    </div>
+      <>
+        <div>
+          <img className="h-8 w-8 rounded-full" src="/img/profile.png" alt="profile" />
+          <h6>Name Surname</h6>
+          <span>Email</span>
+          <span>Company Name</span>
+        </div>
+        <div>
+          <div>
+            Profile details
+          </div>
+        </div>
+      </>
+    </>
   );
 }
 
