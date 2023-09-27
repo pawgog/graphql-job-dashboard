64d7c4239366962dba7f203c064e02186a1ea7f9
pawgog <pgogola10@gmail.com>
2023-09-27 21:28:12 +0200

    Edit profile page details

--- client/src/components/Login/ProfilePage.js
+++ client/src/components/Login/ProfilePage.js
@@ -2,17 +2,36 @@ function ProfilePage() {
   return (
     <>
       <h2 className="mb-14 text-2xl font-bold">Profile Page</h2>
-      <div className="flex h-80">
-        <div className="flex flex-col w-1/2">
-          <img className="h-8 w-8 rounded-full" src="/img/profile.png" alt="profile" />
-          <h6>Name Surname</h6>
-          <span>Email</span>
+      <div className="flex h-auto">
+        <div className="flex flex-col justify-items-center items-center w-1/2 p-2">
+          <img className="h-40 w-40 rounded-full" src="/img/profile.png" alt="profile" />
+          <h6 className="pt-5">Name Surname</h6>
+          <span className="text-sm my-2">user@user.com</span>
           <span>Company Name</span>
         </div>
         <div className="w-1/2">
-          <div>
-            Profile details
-          </div>
+          <table>
+            <tr>
+              <td>Name</td>
+              <td>Name Surname</td>
+            </tr>
+            <tr>
+              <td>Role</td>
+              <td>User</td>
+            </tr>
+            <tr>
+              <td>Email</td>
+              <td>user@user.com</td>
+            </tr>
+            <tr>
+              <td>Contact</td>
+              <td>0123456789</td>
+            </tr>
+            <tr>
+              <td>Status</td>
+              <td>Active</td>
+            </tr>
+          </table>
         </div>
       </div>
     </>
