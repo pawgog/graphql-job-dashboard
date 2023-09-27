119236a3c3b5d90e1f260c5925216f14a4a79645
pawgog <pgogola10@gmail.com>
2023-09-27 21:09:35 +0200

    Edit profile page

--- client/src/components/Login/ProfilePage.js
+++ client/src/components/Login/ProfilePage.js
@@ -1,20 +1,20 @@
 function ProfilePage() {
   return (
     <>
-      <h2>Profile Page</h2>
-      <>
-        <div>
+      <h2 className="mb-14 text-2xl font-bold">Profile Page</h2>
+      <div className="flex h-80">
+        <div className="flex flex-col w-1/2">
           <img className="h-8 w-8 rounded-full" src="/img/profile.png" alt="profile" />
           <h6>Name Surname</h6>
           <span>Email</span>
           <span>Company Name</span>
         </div>
-        <div>
+        <div className="w-1/2">
           <div>
             Profile details
           </div>
         </div>
-      </>
+      </div>
     </>
   );
 }
