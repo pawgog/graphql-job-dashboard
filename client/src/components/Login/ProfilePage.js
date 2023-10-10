fcc1f0aaf65a33b8bb5a58a4a65c3df7d13bace7
pawgog <pgogola10@gmail.com>
2023-10-10 20:35:58 +0200

    Edit user page

--- client/src/components/Login/ProfilePage.js
+++ client/src/components/Login/ProfilePage.js
@@ -4,6 +4,8 @@ const userData = [{
   name: "Name Surname",
   email: "user@user.com",
   phone: "0123456789",
+  company: "Company Name",
+  img: "/img/profile.png",
   role: "User",
   status: "active"
 }]
@@ -13,12 +15,14 @@ function ProfilePage() {
     <>
       <h2 className="mb-14 text-2xl font-bold">Profile Page</h2>
       <div className="flex h-auto">
-        <div className="flex flex-col justify-items-center items-center w-1/2 p-2">
-          <img className="h-40 w-40 rounded-full" src="/img/profile.png" alt="profile" />
-          <h6 className="pt-5">Name Surname</h6>
-          <span className="text-sm my-2">user@user.com</span>
-          <span>Company Name</span>
-        </div>
+        {userData.map((user) => (
+          <div key={user.name} className="flex flex-col justify-items-center items-center w-1/2 p-2">
+            <img className="h-40 w-40 rounded-full" src={user.img} alt="profile" />
+            <h6 className="pt-5">{user.name}</h6>
+            <span className="text-sm my-2">{user.email}</span>
+            <span>{user.company}</span>
+          </div>
+        ))}
         <div className="flex flex-col w-1/2">
           <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
